aa92bf2435558d8fb645ae078eacd43ea50aad1a
Piki Rahmadi <C100720009@bankmandiri.co.id>
2022-09-16 01:06:19 +0700

    update layout v1.3

--- src/App.js
+++ src/App.js
@@ -22,20 +22,20 @@ const App = () => {
     }
     document.onclick = (e) => {
       if (e.target.classList.contains('sidebar-wrapper') || e.target.classList.contains('menu-item')) {
-        setIsShow(!isShow)
+        setIsShow(false);
       }
     }
   });
 
   return (
     <BrowserRouter>
-      <main className="dark:bg-slate-800 dark:text-white">
+      <main className="dark:bg-slate-900 dark:text-white/20 before:bg-[url(./assets/background.png)] before:bg-contain before:bg-no-repeat md:before:bg-right-top dark:before:opacity-30 before:w-[100vw] before:h-[100vh] before:absolute before:top-0 before:left-0">
         <Header />
         <div className="container max-w-full flex min-h-[calc(100vh_-_4rem)]">
-          <div className={`sidebar-wrapper w-full absolute inset-y-0 transition-all ${isShow ? 'bg-black/20 backdrop-blur-sm left-0' : '-left-full'} md:w-auto md:relative md:left-0 md:border-r md:border-primary/10`}>
+          <div className={`sidebar-wrapper w-full fixed inset-y-0 transition-all z-30 md:z-0 ${isShow ? 'bg-slate-600/20 backdrop-blur-sm left-0' : '-left-full'} md:w-auto md:left-0 md:top-[65px] md:bottom-0 md:border-r md:border-primary/10`}>
             <Sidebar isShow={isShow}/>
           </div>
-          <div className="flex-1 p-6 md:px-10">
+          <div className="flex-1 p-6 md:px-10" style={{"height":"2000px"}}>
             <Routes>
               <Route path="/docs" element={<Documentation />} />
               <Route path="/component" element={<Component />} />
