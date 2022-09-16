8ef41c2131e884de8c17cc46214d757ae78b17c2
Piki Rahmadi <C100720009@bankmandiri.co.id>
2022-09-16 15:30:52 +0700

    update layout v1.6 - rapih rapiiih

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className='search-wrapper bg-slate-900/20 backdrop-blur-[2px] fixed inset-0 p-6 flex justify-center items-center z-[999]'>
-      <div className='w-full md:w-1/2 max-h-full overflow-auto bg-white shadow-sm rounded-lg py-2'>
-        <div className='flex justify-between items-center w-full border-b border-primary/10 h-10 px-4'>
-            <MagnifyingGlassIcon className='w-6 h-6 text-primary' />
+    <div className='search-popup'>
+      <div className='search-box'>
+        <div className='search-box-header'>
+            <MagnifyingGlassIcon className='icon' />
         </div>        
       </div>
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = (param) => {
       category: "getting started",
       listMenu: [
         ["introduction", "/intro"],
-        ["documentation", "/documentation"]
+        ["documentation", "/documentation"],
       ],
     },
     {
@@ -24,23 +24,21 @@ const Sidebar = (param) => {
 
   return (
     <aside
-      className={`w-3/4 h-full px-4 py-14 bg-white dark:bg-slate-900 relative ${param.isShow ? "origin-left ease-in-out duration-300 delay-300 left-0" : "-left-full"} md:w-[240px] md:left-0 md:px-6 md:py-10 lg:w-[260px] lg:px-10`}
+      className={`${ param.isShow ? "origin-left ease-in-out duration-300 delay-300 left-0" : "-left-full" } md:left-0`}
     >
-      <div className="absolute right-4 top-4 cursor-pointer close-menu md:hidden">
-        <XMarkIcon className="h-6 w-6 text-current hover:text-primary" />
+      <div className="close-menu">
+        <XMarkIcon />
       </div>
       <ul>
         {menus.map((menu) => (
-          <li key={key += 1} className="mb-6 relative before:w-[1px] before:bg-slate-200 dark:before:bg-slate-800 before:absolute before:top-1 before:bottom-0 before:left-1">
-            <h6 className="text-xs capitalize mb-3 flex items-center gap-2 text-slate-400 before:w-2 before:h-2 before:bg-slate-200 dark:before:bg-slate-800 before:rounded-full">
-              {menu.category}
-            </h6>
+          <li key={(key += 1)}>
+            <h6>{menu.category}</h6>
             <ul>
               {menu.listMenu.map(([title, url]) => (
-                <li key={key += 1} >
+                <li key={(key += 1)}>
                   <NavLink
                     to={url}
-                    className="menu-item capitalize h-8 mb-2 w-full flex items-center px-5 relative before:absolute before:top-0 before:left-[3px] before:w-[3px] before:h-8 before:bg-primary before:hidden hover:before:block dark:text-white"
+                    className="menu-item"
                     category={menu.category}
                   >
                     {title}
